64fb36565b4388d7cdfb37e26ba31abb3978751b
boomjoose <63460978+boomjoose@users.noreply.github.com>
2020-04-10 10:30:02 -0500

    Add files via upload

---
+++ Assets/Move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Vector3 direction = Vector3.forward;
+    public float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.rotation*(direction.normalized * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/MoveLeftAndRight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class MoveLeftAndRight : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+    }
+}
